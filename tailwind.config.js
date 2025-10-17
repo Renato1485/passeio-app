fcea3e4d3da97d7d0fdc74ce0cc25575d488db58
Renato Pontes <renato_pianno@hotmail.com>
2025-10-17 17:46:14 -0300

    finish valid/form

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}' // Ajustar segundo a estrutura do projeto 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
